92eced327ef016e85f44b3700a923abf5b14b46d
brianddk <brianddk@users.noreply.github.com>
2020-05-26 21:36:34 -0500

    corrected repo link

--- python/cw-pwned.py
+++ python/cw-pwned.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # [rights]  Copyright 2020 brianddk at github https://github.com/brianddk
 # [license] Apache 2.0 License https://www.apache.org/licenses/LICENSE-2.0
-# [repo]    github.com/brianddk/reddit/blob/master/tipjar/cw-pwned.py
+# [repo]    github.com/brianddk/reddit/blob/master/python/cw-pwned.py
 # [btc]     BTC-b32: bc1qwc2203uym96u0nmq04pcgqfs9ldqz9l3mz8fpj
 # [tipjar]  github.com/brianddk/reddit/blob/master/tipjar/tipjar.txt
 # [ref]     reddit.com/r/CryptoCurrency/comments/gqrf11/_/frxsy8b
