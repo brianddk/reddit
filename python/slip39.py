8b40fe2b66a526f87877171936b79be4dc31944f
brianddk <brianddk@users.noreply.github.com>
2020-05-30 20:45:30 -0500

    streamlined slip39 flow

--- python/slip39.py
+++ python/slip39.py
@@ -22,47 +22,37 @@ ACCT84 = '84H/0H/0H'
 HW49 = dict(bip32_pub_prefix_hex="049d7cb2", bip32_prv_prefix_hex="049d7878")
 HW84 = dict(bip32_pub_prefix_hex="04b24746", bip32_prv_prefix_hex="04b2430c")
 
-# Can be done MS -> shamir-mnemonic or shamir-mnemonic -> MS
-SSS  = generate_mnemonics(1, [(1,1)], bytes.fromhex(MSEC), PP.encode(), 0)
-#SSS = [["armed husband academic academic document aquatic wisdom " +
-#        "pleasure lilac response axle parking shaft crazy cargo " +
-#        "dish diet dramatic together unfold"]]
-seed = combine_mnemonics(SSS[-1],PP.encode())
+def print_keys(seed, path, kw):
+    if kw:
+        net = btcnet("", "", "", **kw)
+    else:
+        net = btc
+    acct = net.keys.bip32_seed(seed).subkey_for_path(path)
+    key  = acct.subkey_for_path('0/0')
+    hash = key.hash160(is_compressed=True)
+    if path[0:2] == "44":
+        addr = key.address()
+    if path[0:2] == "49":
+        script = btc.contract.for_p2pkh_wit(hash)
+        addr   = btc.address.for_p2s(script)
+    if path[0:2] == "84":
+        addr   = btc.address.for_p2pkh_wit(hash)   
+    xpub = acct.hwif(as_private=False)
+    xprv = acct.hwif(as_private=True)
+    print(f"\n{xprv}\n{xpub}\n{addr}")
 
-print(seed.hex())
-print(SSS[-1][-1], "\n")
+def main(seed):
+    print(seed.hex())
+    print(SSS[-1][-1])
+    print_keys(seed, ACCT44, None)
+    print_keys(seed, ACCT49, HW49)
+    print_keys(seed, ACCT84, HW84)
 
-# BIP44 derivations and xpub/xprv prefix
-acct44 = btc.keys.bip32_seed(seed).subkey_for_path(ACCT44)
-key44  = acct44.subkey_for_path('0/0')
-addr44 = key44.address()
-xprv44 = acct44.hwif(as_private=True)
-xpub44 = acct44.hwif(as_private=False)
-print(xprv44)
-print(xpub44)
-print(addr44, "\n")
-
-# BIP49 derivations and ypub/yprv prefix
-btc49  = btcnet("", "", "", **HW49)
-acct49 = btc49.keys.bip32_seed(seed).subkey_for_path(ACCT49)
-key49  = acct49.subkey_for_path('0/0')
-hash49 = key49.hash160(is_compressed=True)
-scpt49 = btc.contract.for_p2pkh_wit(hash49)
-addr49 = btc.address.for_p2s(scpt49)
-xprv49 = acct49.hwif(as_private=True)
-xpub49 = acct49.hwif(as_private=False)
-print(xprv49)
-print(xpub49)
-print(addr49, "\n")
-
-# BIP84 derivations and zpub/zprv prefix
-btc84  = btcnet("", "", "", **HW84)
-acct84 = btc84.keys.bip32_seed(seed).subkey_for_path(ACCT84)
-key84  = acct84.subkey_for_path('0/0')
-hash84 = key84.hash160(is_compressed=True)
-addr84 = btc.address.for_p2pkh_wit(hash84)
-xprv84 = acct84.hwif(as_private=True)
-xpub84 = acct84.hwif(as_private=False)
-print(xprv84)
-print(xpub84)
-print(addr84)
+if __name__ == "__main__":
+    # Can be done MS -> shamir-mnemonic or shamir-mnemonic -> MS
+    SSS  = generate_mnemonics(1, [(1,1)], bytes.fromhex(MSEC), PP.encode(), 0)
+    # SSS = [["award upgrade academic academic axle mama mason "+
+    #         "walnut invasion security episode prevent scene "+
+    #         "lobe furl exact friar oven judicial improve"]]
+    seed = combine_mnemonics(SSS[-1],PP.encode())
+    main(seed)
