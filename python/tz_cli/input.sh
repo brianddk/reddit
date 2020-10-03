0879c9646546ddd62cdb76d2bbde92b0094dcd0d
brianddk <brianddk@users.noreply.github.com>
2020-10-03 13:43:35 -0500

    Added cli examples

---
+++ python/tz_cli/input.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+secho() {
+  sleep 1
+  echo $*
+}
+secho "Testnet"         # coin name
+secho "tbtc1.trezor.io" # blockbook server and outpoint (below)
+secho "e294c4c172c3d87991b0369e45d6af8584be92914d01e3060fad1ed31d12ff00:0"
+secho "m/84'/1'/0'/0/0" # prev_out derivation to signing key
+secho "4294967293"      # Sequence for RBF; hex(-3)
+secho "segwit"          # Signature type on prev_out to use
+secho ""                # NACK to progress to outs
+secho "2MsiAgG5LVDmnmJUPnYaCeQnARWGbGSVnr3"        # out[0].addr
+secho "10000000"                                   # out[1].amt
+secho "tb1q9l0rk0gkgn73d0gc57qn3t3cwvucaj3h8wtrlu" # out[1].addr
+secho "20000000"                                   # out[1].amt
+secho "tb1qejqxwzfld7zr6mf7ygqy5s5se5xq7vmt96jk9x" # out[2].addr
+secho "99999694"                                   # out[2].amt
+secho ""                # NACK to progress to change
+secho ""                # NACH to skip change
+secho "2"               # txn.version
+secho "0"               # txn.locktime
