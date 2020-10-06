c300044df4ac9b879af3fa6827477184626a8dc3
brianddk <brianddk@users.noreply.github.com>
2020-10-06 18:54:20 -0500

    Requires trezorctl version 0.12.* but build_tx.py version 0.13.*

--- python/tz_cli/csign.sh
+++ python/tz_cli/csign.sh
@@ -5,25 +5,32 @@
 # [ref]     reddit.com/r/TREZOR/comments/j4lp9g/
 # [btc]     BTC-b32: bc1qwc2203uym96u0nmq04pcgqfs9ldqz9l3mz8fpj
 # [tipjar]  github.com/brianddk/reddit/blob/master/tipjar/tipjar.txt
+# [note]    This `build_tx.py` from v0.13.0 but trezorctl v0.12.2
 
 source <(gpg2 -dq environ.asc)
 # source ... (python venv)
 # build_tx=... (path to build script)
 
 test_version() {
+  # Requires FW v2.3.* (or v1.9.*)
   echo "$(md5sum <<DOC
     major_version: 2,
     minor_version: 3,
-    patch_version: 0,
 DOC
 )"
 }
 
 verify_version() {
-  trezorctl get-features | grep version | md5sum --check <( test_version ) > /dev/null
+  trezorctl get-features | grep 'm.*version' | md5sum --check <( test_version ) > /dev/null
 }
 
-if verify_version then
+verify_build() {
+  # Requires build_tx.py version 0.13.* from github `master`
+  build_py="$(cut -f 2 -d ' ' <<< "$build_tx")"
+  grep Decimal $build_py > /dev/null
+}
+
+if verify_version && verify_build
 then
   stxn="$((./input.sh | $build_tx | trezorctl btc sign-tx -) 2> /dev/null)"
 fi
