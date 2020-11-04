f75b6497e1723d83a61f48d3350084f10f5cfdee
brianddk <brianddk@users.noreply.github.com>
2020-11-04 02:15:01 -0600

    Added make-address electrum script

--- python/elec-make-addr.py
+++ python/elec-make-addr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # [rights]  Copyright 2020 brianddk at github https://github.com/brianddk
 # [license] Apache 2.0 License https://www.apache.org/licenses/LICENSE-2.0
-# [repo]    github.com/brianddk/reddit/blob/master/python/elec-get-addr.py
+# [repo]    github.com/brianddk/reddit/blob/master/python/elec-make-addr.py
 # [btc]     BTC-b32: bc1qwc2203uym96u0nmq04pcgqfs9ldqz9l3mz8fpj
 # [tipjar]  github.com/brianddk/reddit/blob/master/tipjar/tipjar.txt
 # [ref]     reddit.com/r/Bitcoin/comments/jnrzcc/-/gb3hjqv/
